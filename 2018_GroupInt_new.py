87feb559a16b38e4d9ad4021d79045f2e82b7f77
Justin Craycraft <jpcraycraft@gmail.com>
2020-10-12 12:01:58 -0500

    initial commit unedited

---
+++ 2018_GroupInt_new.py
@@ -0,0 +1,251 @@
+import numpy as np
+import pandas as pd
+import datetime
+import sys
+import time
+import locale
+
+##########################################################################################################################
+#VPS file includes patient CSN and Intubation Cycles (each CSN has >=1 Intubation Cycle)
+#MAR file includes patient CSN and Pharma data (each CSN has >=1 Pharma record)
+#   Read both files as input:
+#       1) For each CSN, create unique Intubation Cycles 1 thru N for N number of Intubation Cycles
+#       2) Join the Pharma table to Intubation Cycles table, aligning the a TakenTime with Start/End of Intubation Cycles
+##########################################################################################################################
+
+# Date and Time stamp for output file
+dateTimeStamp = time.strftime("%Y%m%d_%H%M")
+
+#file handlers
+VPS_pharma = './Test2.csv'
+print ("pharmacy data loaded")
+VPS = './VPS_test.xls'
+print ("clinical data loaded")
+
+# create dataframes
+#VPS_pharma_DF=pd.read_excel(VPS_pharma)
+VPS_pharma_DF=pd.read_csv(VPS_pharma)
+VPS_DF=pd.read_excel(VPS)
+print ('dataframes read')
+
+#convert date/time format
+VPS_pharma_DF['takenTime_new']=pd.to_datetime(VPS_pharma_DF['TAKEN_TIME'])
+print ('take time read')
+VPS_DF['startTime_new']=pd.to_datetime(VPS_DF['Intubation Start Date/Time'])
+print ('start time read')
+VPS_DF['endTime_new']=pd.to_datetime(VPS_DF['Intubation End Date/Time'])
+print ('Endtime read')
+
+#create additional columns
+VPS_pharma_DF['time_tf'] = np.NaN
+VPS_pharma_DF['IntubationCycle'] = np.NaN
+
+#type cast CSN to a string
+VPS_DF['CSN'] = VPS_DF['CSN'].astype('str')
+VPS_pharma_DF['PAT_CSN'] = VPS_pharma_DF['PAT_CSN'].astype('str')
+
+
+#type cast CSN to a string
+#VPS_DF['CSN'] = VPS_DF['CSN'].astype('str')
+#VPS_pharma_DF['PAT_CSN'] = VPS_pharma_DF['PAT_CSN'].astype('str')
+
+#ID unique CSN's for each DF
+CSN_VPS = VPS_DF['CSN'].unique().tolist()
+print (CSN_VPS)
+CSN_VPS_Pharma = VPS_pharma_DF['PAT_CSN'].unique().tolist()
+print (CSN_VPS_Pharma)
+print ("---------------")
+
+
+# #Testing (Nov 6th, 2017):  Filtering on just a few CSN -- begin
+# csn_filter = ['849331839','848674978','849409396']
+# #create subframe of DF_Null, where CSN that have only 1 IntubationCycle available
+# VPS_DF = VPS_DF.loc[VPS_DF['CSN'].isin(csn_filter),:].copy()
+# VPS_pharma_DF = VPS_pharma_DF.loc[VPS_pharma_DF['PAT_CSN'].isin(csn_filter),:].copy()
+# #ID unique CSN's for each DF
+# CSN_VPS = VPS_DF['CSN'].unique().tolist()
+# CSN_VPS_Pharma = VPS_pharma_DF['PAT_CSN'].unique().tolist()
+# print CSN_VPS
+# print CSN_VPS_Pharma
+# #Testing (Nov 6th, 2017):  Filtering on just a few CSN -- end
+
+
+#VPS file
+#create unique IntubationCycle
+for CSN_VPS_Value in CSN_VPS:
+    tempDF1 = VPS_DF[VPS_DF['CSN']==CSN_VPS_Value]
+    count=1
+    for index, row in tempDF1.iterrows():
+        VPS_DF.loc[index,'IntubationCycle'] = VPS_DF.loc[index,'CSN'] + "-" + str(count)
+        count+=1
+
+#Pharma file
+#loop thru each CSN in Intubation, and group pharma taken times
+for CSN_VPS_Value in CSN_VPS:
+
+    #Filter on unique cycles for each CSN
+    IntubationCycle = VPS_DF[VPS_DF['CSN']==CSN_VPS_Value]['IntubationCycle'].unique().tolist()
+
+    #loop thru each Cycle, compare dates
+    for IntubationCycle_Value in IntubationCycle:
+
+        start = VPS_DF[VPS_DF['IntubationCycle']==IntubationCycle_Value]['startTime_new'].values[0] - np.timedelta64(1, 'h')
+        end = VPS_DF[VPS_DF['IntubationCycle']==IntubationCycle_Value]['endTime_new'].values[0] + np.timedelta64(1, 'h')
+
+        #filter VPSpharma DF based on CSN = CSN_VPS_Value
+        tempDF2 = VPS_pharma_DF[VPS_pharma_DF['PAT_CSN']==CSN_VPS_Value]
+        for index, row in tempDF2.iterrows():
+            taken = VPS_pharma_DF.loc[index,'takenTime_new']
+            if (taken > start) & (taken < end):
+                VPS_pharma_DF.loc[index, 'IntubationCycle'] = IntubationCycle_Value
+
+
+#create slices for complex, easy, omit cohorts where IntubationCycle is NULL
+#create sub DF consisting of 39k records where IntubationCycle is NULL (at threshold above of +/- 1 hour)
+DF_Null = VPS_pharma_DF.loc[VPS_pharma_DF['IntubationCycle'].isnull(),:].copy()
+#indicate that there wasn't a match to IntubationCycle
+DF_Null['Cycle_Match'] = "No"
+
+#save not NULL DF for later
+DF_NotNull = VPS_pharma_DF.loc[VPS_pharma_DF['IntubationCycle'].notnull(),:].copy()
+#indicate that there was a match to IntubationCycle
+DF_NotNull['Cycle_Match'] = "Yes"
+
+
+
+#grab all unique CSN from DF_Null
+csn_null = DF_Null['PAT_CSN'].unique().tolist()
+
+#create list of CSN that have > 1 IntubationCycle available
+csn_complex = []
+for csn_val in csn_null:
+    if len(VPS_DF[VPS_DF['CSN']==csn_val].index) >1:
+        csn_complex.append(csn_val)
+#create subframe of DF_Null_complex, where CSN that have > 1 IntubationCycle available
+DF_Null_complex = DF_Null.loc[DF_Null['PAT_CSN'].isin(csn_complex),:].copy()
+
+#create list of CSN that do NOT have an IntubationCycle available
+csn_omit = []
+for csn_val in csn_null:
+    if len(VPS_DF[VPS_DF['CSN']==csn_val].index) == 0:
+        csn_omit.append(csn_val)
+#create subframe of DF_Null_omit, where CSN do NOT have an IntubationCycle available
+DF_Null_omit = DF_Null.loc[DF_Null['PAT_CSN'].isin(csn_omit),:].copy()
+
+#create list of CSN that have only 1 IntubationCycle available
+csn_easy = []
+for csn_val in csn_null:
+    if len(VPS_DF[VPS_DF['CSN']==csn_val].index) == 1:
+        csn_easy.append(csn_val)
+#create subframe of DF_Null_easy, where CSN that have only 1 IntubationCycle available
+DF_Null_easy = DF_Null.loc[DF_Null['PAT_CSN'].isin(csn_easy),:].copy()
+
+print ('entering loop', csn_val, csn_complex)
+#process complex
+#loop thru each CSN in Intubation, and group pharma taken times
+for csn_val in csn_complex:
+
+    #create subframe for Intubation Cycles filtering on CSN
+    Sub_VPS_DF = VPS_DF.loc[VPS_DF['CSN']==csn_val,:].copy()
+
+    #grab indexes to iterate against for given CSN
+    csn_idx = DF_Null_complex[DF_Null_complex['PAT_CSN']==csn_val].index.tolist()
+
+    print ('entering 2nd loop')
+    #loop thru each csn_idx, calc and compare deltas of takenTime vs IntStartTime
+    for csn_idx_val in csn_idx:
+
+        #grab takenTime value (as scalar value) and set entire column to value
+        time_tmp = DF_Null[DF_Null.index==csn_idx_val]['takenTime_new'].values[0]
+        Sub_VPS_DF['time_tmp'] = time_tmp
+
+        #calc delta between takenTime and Intubation startTime
+        Sub_VPS_DF['delta_tmp'] = Sub_VPS_DF['startTime_new'] - Sub_VPS_DF['time_tmp']
+        #convert to absolute values
+        Sub_VPS_DF['delta_tmp'] = Sub_VPS_DF['delta_tmp']
+
+        #find min delta value and index
+        delta_tmp = Sub_VPS_DF['delta_tmp'].min()
+        #print (delta_tmp)
+        index_tmp = Sub_VPS_DF['delta_tmp'].idxmin()
+        #print (index_tmp)
+        #grab IntubationCycle (as scalar value)
+        cycle_tmp = Sub_VPS_DF[Sub_VPS_DF.index==index_tmp]['IntubationCycle'].values[0]
+        #print (cycle_tmp)
+        #set intubation cycle in master dataframe
+        DF_Null_complex.loc[csn_idx_val,'IntubationCycle'] = cycle_tmp
+        #set delta in master dataframe
+        DF_Null_complex.loc[csn_idx_val,'GroupInt_Delta'] = delta_tmp
+
+#type cast for GroupInt_Delta
+DF_Null_complex.loc[:,'GroupInt_Delta'] = DF_Null_complex.loc[:,'GroupInt_Delta'].astype('timedelta64[D]')
+DF_Null_complex.loc[:,'GroupInt_Delta'] = DF_Null_complex.loc[:,'GroupInt_Delta'].astype('int')
+
+#process easy
+#loop thru each CSN in Intubation, and group pharma taken times
+for csn_val in csn_easy:
+
+    #grab start time from VPS dataframe (as scalar value)
+    startTime_tmp = VPS_DF[VPS_DF['CSN']==csn_val]['startTime_new'].values[0]
+
+    #grab indexes for given CSN from DF_Null dataframe
+    csn_idx = DF_Null[DF_Null['PAT_CSN']==csn_val].index.tolist()
+
+    #set column to startTime in DF_Null dataframe
+    DF_Null_easy.loc[csn_idx,'time_tmp'] = startTime_tmp
+
+    #Found bug
+    #DF_Null_easy['delta_tmp'] = DF_Null_easy['time_tmp'] - DF_Null_easy['time_tmp']
+    #End of bug
+
+    #Fixed on Nov 6th, 2017
+    #***this should be delta between takenTime_new (Pharma data) and startTime_new (IntubationCycle data)****
+    DF_Null_easy['delta_tmp'] = DF_Null_easy['takenTime_new'] - DF_Null_easy['time_tmp']
+    DF_Null_easy['GroupInt_Delta'] = DF_Null_easy['delta_tmp']
+
+    #convert to absolute values
+    DF_Null_easy['GroupInt_Delta'] = DF_Null_easy['GroupInt_Delta']
+
+    #grab IntubationCycle (as scalar value)
+    cycle_tmp = VPS_DF[VPS_DF['CSN']==csn_val]['IntubationCycle'].values[0]
+
+    #set intubation cycle in DF_Null dataframe
+    DF_Null_easy.loc[csn_idx,'IntubationCycle'] = cycle_tmp
+
+    #set delta in DF_Null dataframe
+    DF_Null_easy['GroupInt_Delta'] = DF_Null_easy['delta_tmp']
+
+#remove temporary fields from DF_Null_easy
+fields = ['delta_tmp','time_tmp']
+DF_Null_easy = DF_Null_easy.drop(fields, axis=1)
+#type cast for GroupInt_Delta
+DF_Null_easy.loc[:,'GroupInt_Delta'] = DF_Null_easy.loc[:,'GroupInt_Delta'].astype('timedelta64[D]')
+DF_Null_easy.loc[:,'GroupInt_Delta'] = DF_Null_easy.loc[:,'GroupInt_Delta'].astype('int')
+
+
+#define DF_Null_out
+DF_Null_out = pd.DataFrame()
+#append DF_Null_complex to output
+DF_Null_out = DF_Null_out.append(DF_Null_complex, ignore_index=True)
+#append DF_Null_easy to output
+DF_Null_out = DF_Null_out.append(DF_Null_easy, ignore_index=True)
+#append DF_Null_omit to output
+DF_Null_out = DF_Null_out.append(DF_Null_omit, ignore_index=True)
+#append DF_Null with DF_NotNull
+outputDF = DF_Null_out.append(DF_NotNull, ignore_index=True)
+
+# #convert to absolute values
+# outputDF['GroupInt_Delta'] = outputDF['GroupInt_Delta'].abs()
+
+#fill null/empty cells with -1 for Tableau filter
+outputDF['GroupInt_Delta'] = outputDF['GroupInt_Delta'].fillna(-1)
+
+
+#left join of dataframes
+joinedDF = pd.merge(outputDF, VPS_DF, on='IntubationCycle', how='left')
+
+#add rowID field
+joinedDF['rowID']=joinedDF.index + 1
+
+#output to CSV
+joinedDF.to_csv(dateTimeStamp+'_'+'COMBO_joined_CSN.csv', index = False, encoding='utf-8')
---
+++ 2018_Pharm_continuous.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import numpy as np
+import datetime
+import sys
+import time
+import locale
+
+#locale.setlocale(locale.LC_ALL, '')
+
+print ("data prep starting...")
+
+# Start stopwatch
+startTime = time.process_time()
+
+# Date and Time stamp for output file
+dateTimeStamp = time.strftime("%Y%m%d_%H%M")
+
+
+def groupCSN(df):
+
+    #Creat CSN_group field and set it with Pat_CSN values
+    cleanDF['CSN_group'] = cleanDF['PAT_CSN']
+    #Create a list of MRN's that have Admsn=Null or Admsn='1/0/00 0:00'
+    uniqueMRN = np.unique(np.concatenate((cleanDF[pd.isnull(cleanDF['HOSP_ADMSN_TIME'])==True]['MRN'].unique(),cleanDF[cleanDF['HOSP_ADMSN_TIME'].astype(str) == '1/0/00 0:00']['MRN'].unique()),axis=0))
+    #Cycle through each of the MRN's and set CSN_group if Admsn=Null or Admsn='1/0/00 0:00' CSN's align within the action dates of another CSN for same MRN
+    for mrnValue2 in uniqueMRN:
+        #process only those MRN that have >1 Pat_CSN
+        if len(cleanDF[cleanDF['MRN']==mrnValue2]['PAT_CSN'].unique()) > 1: 
+            #CSN list #1: uniques (Null=True) or (1/0/00; aka Null)
+            uniqueCSN_null_1 = cleanDF[(pd.isnull(cleanDF['HOSP_ADMSN_TIME'])==True) & (cleanDF['MRN']==mrnValue2)]['PAT_CSN'].unique()
+            uniqueCSN_null_2 = cleanDF[(cleanDF['HOSP_ADMSN_TIME'].astype(str) == '1/0/00 0:00') & (cleanDF['MRN']==mrnValue2)]['PAT_CSN'].unique()
+            uniqueCSN_null = np.unique(np.concatenate((uniqueCSN_null_1,uniqueCSN_null_2),axis=0))
+            #CSN list #2:  uniques (Null=False)
+            uniqueCSN = cleanDF[(pd.isnull(cleanDF['HOSP_ADMSN_TIME'])==False) & (cleanDF['MRN']==mrnValue2) & (cleanDF['HOSP_ADMSN_TIME'].astype(str) != '1/0/00 0:00')]['PAT_CSN'].unique()       
+            #Compare CSN in the Null list with those that have non-null values
+            for csnValue_null in uniqueCSN_null:
+                for csnValue in uniqueCSN:
+                    #If in between range TRUE, then assign CSN_group
+                    first_month = cleanDF[cleanDF['PAT_CSN']==csnValue]['action_new'].dt.month.astype(object).min()
+                    last_month = cleanDF[cleanDF['PAT_CSN']==csnValue]['action_new'].dt.month.astype(object).max()
+                    if df[df['PAT_CSN']==csnValue_null]['action_new'].dt.month.between(first_month, last_month, inclusive=True).any():
+                        cleanDF.loc[cleanDF['PAT_CSN']==csnValue_null,'CSN_group'] = csnValue
+
+    return cleanDF  
+
+
+#input file
+#rawDF=pd.read_csv('/Users/jcraycraft/Dropbox/Coding/Python/TCH/Versions/twb_v7_py_v7_1_2/data/input/BMJan7CICU Sedation 2011-2015.csv')
+
+
+# Identify CSV input and script version
+if len(sys.argv) < 2 :
+    raise NameError('csv filename argument missing')
+inputCSV = sys.argv[1];
+scriptName = sys.argv[0].split(".")
+outputCSV = dateTimeStamp + '_' + scriptName[0] + ".csv"
+
+# Read CSV into dataframe
+
+rawDF=pd.read_csv(inputCSV)
+
+#Add RowID column
+cleanDF = rawDF.copy()
+cleanDF['rowID']=cleanDF.index + 1
+
+#column names / headers mapping
+_input_drug_name_ = 'DESCRIPTION'
+_input_dose_ = 'DOSE'
+_input_dtime_ = 'TAKEN_TIME'
+_input_id_ = 'PAT_CSN'
+_input_cycle_ = 'IntubationCycle'
+# determine if continuous
+_input_freq_ = 'FREQ_NAME'
+#########################################################switch back to all lower case based off abbreviated name; check for Nick's
+
+
+#beginning of edits
+
+#trim string to first word; i.e. grab "normalized" drug name
+cleanDF[_input_drug_name_] = cleanDF[_input_drug_name_].str.split().str.get(0).str.lower()
+
+drug_fam_dict = {"fentanyl" : "Opioid", "hydromorphone": "Opioid", "morphine" : "Opioid", "hydrocodone" : "Opioid", "methadone" : "Opioid", "dexmedetomidine" : "Dex" , "midazolam": "Benzo", "ketamine": "Ketamine", "lorazepam": "Benzo", "propofol": "Propofol", "chloral": "Chloral", "atracurium": "NMB", "vecuronium": "NMB", "rocuronium": "NMB", "cisatracurium": "NMB", "clonidine": "Clonidine", "pentobarbital": "Barb", "phenobarbital": "Barb"}
+cleanDF["drug_fam"] = cleanDF[_input_drug_name_].map(drug_fam_dict) 
+
+#changed Hydro from .15 to .015  
+drug_equiv_dict = {"fentanyl" : .01, "hydromorphone": .015, "morphine" : 1, "dexmedetomidine" : 1, "midazolam": 1, "lorazepam": 0.3, "hydrocodone" : 1.9, "methadone": .2, "ketamine": 1, "propofol": 1, "chloral": 1, "atracurium": 1, "cisatracurium": 1, "vecuronium": 1, "rocuronium": 1, "clonidine": 1, "pentobarbital": 1, "phenobarbital": 1}
+
+#end of edits
+
+
+cleanDF["MSO4equiv"] = cleanDF[_input_drug_name_].map(drug_equiv_dict)
+cleanDF['MSO4equivDOSE'] = cleanDF['MSO4equiv'] * cleanDF[_input_dose_]
+
+cleanDF['action_new']=pd.to_datetime(cleanDF[_input_dtime_])
+
+
+
+#groupCSN(cleanDF)
+
+
+
+
+
+#save non-Narcotic drugs for later
+nonNarcoticDF =cleanDF[cleanDF['drug_fam'].isnull()]
+
+sortedDF = cleanDF.groupby([_input_id_,'drug_fam']).apply(pd.DataFrame.sort_values, 'action_new').reset_index(drop=True)
+
+#test groupby with 'FREQ_NAME'
+
+uniqueEncounters = sortedDF[_input_id_].unique()
+
+
+#try slice again here
+continuousDF = sortedDF.loc[sortedDF[_input_freq_]== 'CONTINUOUS'].copy()
+not_continuousDF = sortedDF.loc[sortedDF[_input_freq_]!= 'CONTINUOUS'].copy()
+
+subAnswer = pd.DataFrame()
+answerDF1 = pd.DataFrame()
+answerDF2 = pd.DataFrame()
+
+for encounterValue in uniqueEncounters:
+    print("------------------------------------------------")
+    #print("Processing encounterValue: " + encounterValue)
+    print("------------------------------------------------")
+    subAnswer = pd.DataFrame()
+    subDF1 = continuousDF.loc[continuousDF[_input_id_]== encounterValue].copy()
+    #subDF1 = continuousDF.loc[continuousDF['EncounterID']== '35-2'].copy()
+
+    uniqueCycles = subDF1[_input_cycle_].unique()
+
+    for cycleValue in uniqueCycles:
+        subDF2 = subDF1.loc[subDF1[_input_cycle_]== cycleValue].copy()
+        uniqueDrug = subDF2[_input_drug_name_].unique()
+        #uniqueDrugFam = subDF2['drug_fam'].unique()
+        
+        #for drug in uniqueDrugFam:
+        for drug in uniqueDrug:
+            print("Processing drug: " + drug)
+            subDF3 = subDF2.loc[subDF2[_input_drug_name_] == drug].copy().reset_index()
+            subDF3['timeDelta']= (subDF3['action_new']-subDF3['action_new'].shift()).fillna(0)
+            subDF3['timeDeltaSec'] = 0
+            subDF3['cycle'] = 1
+            subDF3['cumulativeDose'] = 0
+            #subDF2['MSO4equivDOSE'] = 0
+
+            for row in range(subDF3.shape[0])[1:subDF3.shape[0]]:  ###making assumption that each subdf2 has at least two times
+                subDF3.loc[row, 'timeDeltaSec'] = subDF3.loc[row, 'timeDelta'].seconds
+                #subDF2.loc[row, 'cumulativeDose'] = subDF2.loc[row-1,'MSO4equiv'] * subDF2.loc[row-1,'DOSE'] * subDF2.loc[row, 'timeDeltaSec'] / 3600
+                subDF3.loc[row, 'cumulativeDose'] = subDF3.loc[row-1,'MSO4equivDOSE'] * subDF3.loc[row, 'timeDeltaSec'] / 3600
+                #subDF2.loc[row, 'MSO4equivDOSE'] = subDF2.loc[row,'MSO4equiv'] * subDF2.loc[row,'DOSE']
+                
+        #catching the cycle, but not redefining scope for time delta calc; see 2-1 and 7-1, Mid 2 (11 should be 0)
+
+                if subDF3['timeDelta'][row]>=  datetime.timedelta(hours = 48):            
+                    subDF3.loc[row, 'cycle'] = subDF3.loc[row-1,'cycle'] + 1
+                else:
+                    subDF3.loc[row,'cycle'] = subDF3.loc[row-1,'cycle']
+
+            subAnswer = subAnswer.append(subDF3, ignore_index=True)
+        print("Completed Drug: " + drug )        
+    answerDF1 = answerDF1.append(subAnswer, ignore_index=True)
+answerDF2 = answerDF2.append(answerDF1, ignore_index=True)
+
+answerDF2['cycle_unique'] = answerDF2[_input_id_].astype('str') + "-" + answerDF2['cycle'].astype('str') 
+# append on the non-continous
+answerDF2 = answerDF2.append(not_continuousDF, ignore_index=True)
+#append non-Narcotic drugs 
+answerDF2 = answerDF2.append(nonNarcoticDF, ignore_index=True)
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+print ("data prep finsished...")
+
+# Output elapsed time
+print ("Elapsed:", locale.format("%.2f", time.clock() - startTime), "seconds")
+print("------------------------------------------------")
+print("------------------------------------------------")
+
+
+
+#answerDF.to_csv('/Users/jcraycraft/Dropbox/Coding/Python/TCH/Versions/twb_v7_py_v7_1_2/data/Output/master_out_v7_1_3.csv', index = False)
+# Identify CSV input and script version
+if len(sys.argv) < 2 :
+    raise NameError('csv filename argument missing')
+inputCSV = sys.argv[1];
+scriptName = sys.argv[0].split(".")
+outputCSV = dateTimeStamp + '_' + scriptName[0] + ".csv"
+
+answerDF2.to_csv(outputCSV, index = False)
+
+
+
